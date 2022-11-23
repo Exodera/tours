683d2fc32db2c33cc09dccc53e17b2f6ed6ad0d6
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-11-23 20:26:46 +0200

    Updates

---
+++ precache-manifest.8650e91ff78f9a6c155f65e47a62f7f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48cff804d9c710d0f631923d0e435ff",
+    "url": "/tours/index.html"
+  },
+  {
+    "revision": "4a9b05a898c41853cb0e",
+    "url": "/tours/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9b05a898c41853cb0e",
+    "url": "/tours/static/js/main.7d602aa4.chunk.js"
+  },
+  {
+    "revision": "cabdc42b426759795a4b",
+    "url": "/tours/static/js/runtime-main.3b7494d8.js"
+  }
+]);
